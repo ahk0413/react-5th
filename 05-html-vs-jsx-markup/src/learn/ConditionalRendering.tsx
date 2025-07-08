a1b086fa57bb7922ad5091da59d4569548da1cba
ahk0413 <rladkgusgd@gmail.com>
2025-07-08 18:11:15 +0900

    jsx 렌더링 학습

--- 05-html-vs-jsx-markup/src/learn/ConditionalRendering.tsx
+++ 05-html-vs-jsx-markup/src/learn/ConditionalRendering.tsx
@@ -2,6 +2,7 @@ import reactImagePath from '@/assets/react.svg?url';
 import viteImagePath from '@/assets/vite.svg?url';
 import nextJsImagePath from '@/assets/next-js.svg?url';
 import kakaoTalkImagePath from '@/assets/kakao-talk.svg?url';
+import isTrueOrFalse from '@/utils/isTrueOrFalse';
 
 
 interface Props {
@@ -38,9 +39,13 @@ function ConditionalRendering({imageType}:Props) {
     default:
       printText = '허용된 이미지 타입이 존재하지 않습니다.';
   }
+
+  const tf = isTrueOrFalse();
+  const spinnerOrVite = tf  ? (<img src="/icons/spinner.svg" alt="로딩 중..." />) : (<img src="/vite.svg" alt="vite logo" />)
+
   return (
     <>
-      <dt>조건부 렌더링 (conditinal rendering)</dt>
+      <dt>조건부 렌더링 (conditional rendering) ({tf && '스피너 표시'})</dt>
       <dd>
         <p>이미지 타입(image type)에 따라 렌더링 여부를 결정합니다.</p>
         <div className="conditionalRendering">
@@ -50,8 +55,9 @@ function ConditionalRendering({imageType}:Props) {
       </dd>
       <dd>
         <p>spinner 또는 vite 이미지가 랜덤으로 화면에 렌더링 되도록 합니다.</p>
+        {imageType === 'vite'}
         <div className='conditionalRendering'>
-          <img src="/icons/spinner.svg" alt="" />
+          {spinnerOrVite}
         </div>
       </dd>
     </>
--- 05-html-vs-jsx-markup/src/learn/JSX_Markup.tsx
+++ 05-html-vs-jsx-markup/src/learn/JSX_Markup.tsx
@@ -1,14 +1,27 @@
 
-import ConditionalRendering from './ConditionalRendering'
+import * as learnData from '@/data/learn'
 import DataBinding from './DataBinding'
-import { imageType, statusMessage } from '@/data/learn'
+import ConditionalRendering from './ConditionalRendering'
+import ConditionalDisplay from './ConditionalDisplay'
+import RenderList from './RenderList';
 
 
 function JSX_Markup() {
+
+  const {
+    statusMessage,
+    imageType,
+    isShowReactImage,
+    reactLibrary,
+    statusMessageWithId
+  } = learnData;
+
   return (
     <dl className="descriptionList">
       <DataBinding statusMessage={statusMessage}/>
       <ConditionalRendering imageType={imageType} />
+      <ConditionalDisplay isShowImage={isShowReactImage}/>
+      <RenderList reactLibrary = {reactLibrary} items={statusMessageWithId} />
     </dl>
   )
 }
