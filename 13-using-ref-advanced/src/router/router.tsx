6fb660413100d1bb8aa046c5df4e072b76a1fc85
ahk0413 <rladkgusgd@gmail.com>
2025-07-21 12:01:30 +0900

    using ref 학습중

---
+++ 13-using-ref-advanced/src/router/router.tsx
@@ -0,0 +1,36 @@
+import AccessDom from "@/pages/AccessDom";
+import Home from "@/pages/Home/Home";
+import Remember from "@/pages/Remember";
+
+
+
+
+
+export const routes = [
+  {
+    title:'리액트 애니메이션',
+    path:'/',
+    element: <Home />
+  },
+  {
+    title:'리-렌더 없이 기억하기',
+    path:'/re-render',
+    element: <Remember />
+  },
+  {
+    title: 'DOM 노드 접근 / 조작',
+    path:'/access-dom',
+    element: <AccessDom />
+  },
+]
+
+
+
+
+
+
+
+
+
+
+
