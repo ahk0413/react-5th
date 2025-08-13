379136f91ad038d158653cc4d5faac3bd883bb67
ahk0413 <rladkgusgd@gmail.com>
2025-08-13 15:51:46 +0900

    tailwind css 학습시작

---
+++ 15-style-with-tailwind/src/App.tsx
@@ -0,0 +1,12 @@
+import '/src/styles/style.css';
+
+function App() {
+  return (
+    <div>
+      <h1 className="flex justify-center items-center bg-indigo-500 text-white px-4 py-2 mt-4">
+        hello tailwind
+      </h1>
+    </div>
+  );
+}
+export default App;
