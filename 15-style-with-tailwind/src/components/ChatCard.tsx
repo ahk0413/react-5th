53378f0239a7cebc58f451be18f8c5ae1d55fb25
ahk0413 <rladkgusgd@gmail.com>
2025-08-14 12:52:03 +0900

    twMerge, clsx 학습

---
+++ 15-style-with-tailwind/src/components/ChatCard.tsx
@@ -0,0 +1,16 @@
+function ChatCard() {
+  return (
+    <div className="flex items-center max-w-sm mx-auto p-6 outline-1 gap-x-4 bg-white dark:bg-slate-800 shadow-lg rounded-xl outline-black/5">
+      <img className="size-12" src="/vite.svg" alt="vite logo" />
+      <div>
+        <strong className="text-xl font-medium text-black dark:text-white">
+          ChitChat
+        </strong>
+        <p className="text-gray-500 dark:text-gray-400">
+          you have a new Message
+        </p>
+      </div>
+    </div>
+  );
+}
+export default ChatCard;
